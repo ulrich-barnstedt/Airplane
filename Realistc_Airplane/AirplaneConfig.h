fd5c9785d8617ca52c1c95f8124f2fb6f25d0946
DerCookieLiker <gsupersocke@gmail.com>
2021-03-22 08:49:43 +0100

    Update AirplaneConfig.h

--- Realistc_Airplane/AirplaneConfig.h
+++ Realistc_Airplane/AirplaneConfig.h
@@ -2,7 +2,7 @@
 
 typedef struct Config* PConfig;
 
-PConfig Config_create(float cw, float weight, int wingsCount, int rudderCount, int gearsCount, int turbineCount, int thrustAddVlue, char* callSign);
+PConfig Config_create(float weight, int wingsCount, int rudderCount, int gearsCount, int turbineCount, int thrustAddVlue, char* callSign, float span, float hullDiameter, float length);
 void Config_print(PConfig _this);
 
 void Config_setThrustAddValue(PConfig _this, int value);
@@ -12,5 +12,6 @@ int Config_getWingsCount(PConfig _this);
 int Config_getGearsCount(PConfig _this);
 int Config_getTurbineCount(PConfig _this);
 int Config_getRudderCount(PConfig _this);
-float Config_getCWValue(PConfig _this);
+int Config_getSpan(PConfig _this);
+int Config_getHullDiameter(PConfig _this);
 
