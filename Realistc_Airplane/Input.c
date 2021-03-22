8c4cc5598991e51b37d761347d5d792b0d327957
k05j <kilian.halwachs@gmx.at>
2021-03-22 09:17:37 +0100

    adding basic functions

--- Realistc_Airplane/Input.c
+++ Realistc_Airplane/Input.c
@@ -8,5 +8,13 @@ struct Input {
 };
 
 PInput Input_createInstance() {
-
+    PInput in = (PInput)malloc(sizeof(struct Input));
+    if (!in) {
+        in->choice = '0';
+        in->lastChoice = in->choice;
+    }else {
+        printf("Error occured while allocation. (Input)");
+        return;
+    }
+    return in;
 }
--- Realistc_Airplane/Input.h
+++ Realistc_Airplane/Input.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include "Airplane.h"
+
 typedef struct Input *PInput;
 
 PInput Input_createInstance();
+
+void Input_getUserInput(PInput _this);
+char Input_checkUserInput(PInput _this);
+void Input_callFunctionNeeded(PInput _this, PAirplane _that);
